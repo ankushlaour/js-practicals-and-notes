08591b904ac5689ae0a9be0a2ad8d1ab068cede5
ankush laour <ankushlaour2000@gmail.com>
2024-04-19 18:29:24 +0530

    arrays

---
+++ array.js
@@ -0,0 +1,23 @@
+// JavaScript arrays are resizable and can contain a mix of different data types.
+
+const arr=[9,8,7,6,5,4]
+console.log(arr);
+
+// If you want to know  difference between 
+// slice and splice method,then plese run this code 
+
+// const arr1=arr.slice(0,3);
+// console.log(arr1);
+// console.log(arr);
+// const arr2=arr.splice(0,3);
+// console.log(arr2);
+// console.log(arr)
+
+// push and pop method 
+// push method use to add the element end of the array
+const pusharr=arr.push(1,2,3,4);
+console.log(arr);
+// pop method use to remove the element end of the aeray
+
+const poparr=arr.pop();
+console.log(arr);
